7ac4363633cfa381d7be1034a8a4da14772328f4
sandali-singh <sandalisingh1270@gmail.com>
2024-02-01 14:12:34 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <calculator></calculator>
+  </div>
+</template>
+
+<script>
+import calculator from "./calculator.vue";
+export default {
+  name: "app",
+  components: {
+    calculator,
+  },
+};
+</script>
+
+<style>
+body {
+  background: #643843;
+}
+</style>
